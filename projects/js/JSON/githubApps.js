c3f28a690e0f2849bdd4f9e405c935a81ddbeb72
nigelgalbraith <144597433+nigelgalbraith@users.noreply.github.com>
2026-03-22 22:42:09 +1300

    ..

--- projects/js/JSON/githubApps.js
+++ projects/js/JSON/githubApps.js
@@ -6,12 +6,6 @@ const GITHUB_APPS = {
     playLink: "https://attogram.github.io/pacman-lite/",
     download: "https://codeload.github.com/attogram/pacman-lite/zip/refs/heads/master",
     source: "https://github.com/attogram/pacman-lite"
-  },
-  galaga: {
-    title: "Galaga",
-    icon: "Galaga.png",
-    download: "https://codeload.github.com/gregfrazier/gakaga/zip/refs/heads/master",
-    source: "https://github.com/gregfrazier/gakaga"
   }
   // Add more apps here as needed
 };
